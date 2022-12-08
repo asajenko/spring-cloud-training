7439a7a0d2977b34ac58b7aec40b70784acfecda
landrzejewski <landrzejewski.poczta@gmail.com>
2022-12-08 15:39:32 +0100

    `RC`

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-#./mvnw clean package -DskipTests
+./mvnw clean package -DskipTests
 docker build -t training/admin-server admin-server
+docker build -t training/configuration-server configuration-server
+docker build -t training/discovery-server discovery-server
