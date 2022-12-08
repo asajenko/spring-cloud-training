5b2a002339c6c23e3aac1b129fb2be4e789926d0
landrzejewski <landrzejewski.poczta@gmail.com>
2022-12-08 12:02:15 +0100

    `RC`

--- shop-service/src/main/java/pl/training/shop/SecurityConfiguration.java
+++ shop-service/src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -1,16 +1,33 @@
 package pl.training.shop;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorityAuthorizationManager;
-import org.springframework.security.authorization.AuthorizationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import pl.training.shop.commons.security.BaseSecurityConfiguration;
 
 // https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@KeycloakConfiguration
+public class SecurityConfiguration extends BaseSecurityConfiguration {
 
-        /*
+    @Autowired
+    private RequestMatcher apiMatcher;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().ignoringRequestMatchers(apiMatcher)
+                .and()
+                .authorizeRequests()
+                .mvcMatchers("/**").hasRole("ADMIN")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/index.html");
+    }
+
+    /*
 
     // Aktywacja uwierzytelniania odbywa się przez zestaw filtrów web, albo aspekty nakładane na beany
 
@@ -40,9 +57,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                                            // Podejmuje decyzję na podstawie głosowania AccessDecisionVoter accessDecisionVoter
 
     */
-
-
-
 
 
 }
---
+++ shop-service/src/main/java/pl/training/shop/commons/security/BaseSecurityConfiguration.java
@@ -0,0 +1,62 @@
+package pl.training.shop.commons.security;
+
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationEntryPoint;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+public class BaseSecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    @Override
+    protected KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
+        var keycloakProvider = super.keycloakAuthenticationProvider();
+        keycloakProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        return keycloakProvider;
+    }
+
+    @Bean
+    public RequestMatcher apiMatcher(@Value("apiPath") String apiPath) {
+        return new AntPathRequestMatcher(apiPath);
+    }
+
+    @Override
+    protected AuthenticationEntryPoint authenticationEntryPoint() throws Exception {
+        return new KeycloakAuthenticationEntryPoint(adapterDeploymentContext(), apiMatcher());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration(@Value("${cors.origin}") String origin,
+                                                     @Value("${cors.headers}") List<String> headers,
+                                                     @Value("${cors.methods}") List<String> methods) {
+        var configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin(origin);
+        configuration.setAllowedHeaders(headers);
+        configuration.setAllowedMethods(methods);
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader(HttpHeaders.LOCATION);
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
