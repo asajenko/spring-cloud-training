6d2f90d1ef077c4db1d3c3fe83b67d551def9020
landrzejewski <landrzejewski.poczta@gmail.com>
2022-12-13 12:05:29 +0100

    s

--- warehouse-adapters/src/main/java/pl/training/warehouse/adapters/persistence/JpaProductRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.training.warehouse.adapters.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
-}
--- warehouse-ports/src/main/java/pl/training/warehouse/ports/input/GetProductUseCase.java
+++
@@ -1,9 +0,0 @@
-package pl.training.warehouse.ports.input;
-
-import pl.training.warehouse.ports.model.Product;
-
-public interface GetProductUseCase {
-
-    Product getById(Long id);
-
-}
